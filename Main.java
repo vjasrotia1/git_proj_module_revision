b285bc7dc8de67bb158508a65f548d84d98b3093
Priyanka Hiranandani <priyankahiranandani@Priyankas-MacBook-Air.local>
2026-01-27 09:33:27 +0530

    print statements

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main{
 public static void main(String args[]){
     System.out.println("Hello, World!");
-    System.out.println("This is a test file.");  
+    System.out.println("This is a test file."); 
+    varun singh 
+    roger federer
+    roger binny
 }
 }
